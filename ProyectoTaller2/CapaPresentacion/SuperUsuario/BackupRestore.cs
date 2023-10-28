90cea652076cfea4ec6ab6d4d26b1ac5c8fef51a
AcevedoAriel <arieel.zurdo1993@gmail.com>
2023-10-28 01:21:05 -0300

    Modificacion en el form Backup y Restore

--- ProyectoTaller2/CapaPresentacion/SuperUsuario/BackupRestore.cs
+++ ProyectoTaller2/CapaPresentacion/SuperUsuario/BackupRestore.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using ProyectoTaller2.CapaDatos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,10 +21,10 @@ namespace ProyectoTaller2.CapaPresentacion.SuperUsuario
 
         private void btnAbrirRespaldo_Click(object sender, EventArgs e)
         {
+            saveFileDialog1.Filter = "SQL Backup files|*.bak";
+            saveFileDialog1.FileName = $"{CBBaseDeDatos.Text}-{DateTime.Today.ToString("dd-MM-yyyy")}-{DateTime.Now.ToString("h.mm")}.bak";
             if (saveFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                //Aqui va el codigo para abrir y leer el archivo.
-
                 txtAbrirGuardar.Text = saveFileDialog1.FileName;
             }
         }
@@ -39,30 +41,40 @@ namespace ProyectoTaller2.CapaPresentacion.SuperUsuario
 
         private void btnConectarBD_Click(object sender, EventArgs e)
         {
-
-            if (CBBaseDeDatos.Text == "")
+            using (SqlConnection conexion = Conexion.ObtenerConexion())
             {
-
-                MessageBox.Show("El campo está vacío. Por favor Conecte el Motor de la BD", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            else
-            {
-                // Aqui va el codigo
-
+                SqlCommand cmd = new SqlCommand("SELECT name from sys.databases", conexion);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    foreach (DataRow fila in ds.Tables[0].Rows)
+                    {
+                        CBBaseDeDatos.Items.Add(fila[0].ToString());
+                    }
+                    
+                    CBBaseDeDatos.SelectedIndex = 0;
+                
+                }
             }
         }
 
         private void btnBackup_Click(object sender, EventArgs e)
         {
-            if (txtAbrirGuardar.Text == "")
+            try
             {
+                // Verifica que exista la carpeta, en caso contrario la crea
+                if (!Directory.Exists(@"C:\Users\ariel\OneDrive\Documentos\Backup"))
+                {
+                    Directory.CreateDirectory(@"C:\Users\ariel\OneDrive\Documentos\Backup");
+                }
 
-                MessageBox.Show("El campo está vacío.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                System.Diagnostics.Process.Start("cmd", $"/k sqlcmd -S localhost\\SQLEXPRESS -E -Q \"BACKUP DATABASE [{CBBaseDeDatos.Text}] TO DISK='{txtAbrirGuardar.Text}'\"");
             }
-            else
+            catch (Exception ex)
             {
-                // Aqui va el codigo
-
+                MessageBox.Show(ex.Message);
             }
         }
 
