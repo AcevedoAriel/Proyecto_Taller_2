452b4bf6fd193acd3f2c00e8d38264df6bd8f2bc
AcevedoAriel <arieel.zurdo1993@gmail.com>
2023-10-30 14:10:38 -0300

    Restore y backup solucionado

--- ProyectoTaller2/CapaPresentacion/SuperUsuario/BackupRestore.cs
+++ ProyectoTaller2/CapaPresentacion/SuperUsuario/BackupRestore.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using ProyectoTaller2.CapaDatos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,65 +21,74 @@ namespace ProyectoTaller2.CapaPresentacion.SuperUsuario
 
         private void btnAbrirRespaldo_Click(object sender, EventArgs e)
         {
+            saveFileDialog1.Filter = "SQL Backup files|*.bak";
+            saveFileDialog1.FileName = $"{CBBaseDeDatos.Text}-{DateTime.Today.ToString("dd-MM-yyyy")}-{DateTime.Now.ToString("h.mm")}.bak";
             if (saveFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                //Aqui va el codigo para abrir y leer el archivo.
-
                 txtAbrirGuardar.Text = saveFileDialog1.FileName;
             }
         }
 
         private void btnAbrirRestaurar_Click(object sender, EventArgs e)
         {
-            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                //Aqui va el codigo para Salvar.
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "SQL Backup Files|*.bak";
+            openFileDialog1.FileName = "";
 
-                txtAbrirRestaurar.Text = saveFileDialog1.FileName;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtAbrirRestaurar.Text = openFileDialog1.FileName;
             }
         }
 
         private void btnConectarBD_Click(object sender, EventArgs e)
         {
-
-            if (CBBaseDeDatos.Text == "")
+            using (SqlConnection conexion = Conexion.ObtenerConexion())
             {
+                SqlCommand cmd = new SqlCommand("SELECT name from sys.databases", conexion);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    foreach (DataRow fila in ds.Tables[0].Rows)
+                    {
+                        CBBaseDeDatos.Items.Add(fila[0].ToString());
+                    }
 
-                MessageBox.Show("El campo está vacío. Por favor Conecte el Motor de la BD", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            else
-            {
-                // Aqui va el codigo
+                    CBBaseDeDatos.SelectedIndex = 0;
 
+                }
             }
         }
 
         private void btnBackup_Click(object sender, EventArgs e)
         {
-            if (txtAbrirGuardar.Text == "")
+            try
             {
+                // Verifica que exista la carpeta, en caso contrario la crea
+                if (!Directory.Exists(@"D:\Backup"))
+                {
+                    Directory.CreateDirectory(@"D:\Backup");
+                }
 
-                MessageBox.Show("El campo está vacío.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                System.Diagnostics.Process.Start("cmd", $"/k sqlcmd -S localhost\\SQLEXPRESS -E -Q \"BACKUP DATABASE [{CBBaseDeDatos.Text}] TO DISK='{txtAbrirGuardar.Text}'\"");
             }
-            else
+            catch (Exception ex)
             {
-                // Aqui va el codigo
-
+                MessageBox.Show(ex.Message);
             }
         }
 
         private void btnRestaruar_Click(object sender, EventArgs e)
         {
-            if (txtAbrirRestaurar.Text == "")
-            {
+            string baseName = txtBaseRestaurar.Text;
+            string rutaRestaurar = txtAbrirRestaurar.Text;
 
-                MessageBox.Show("El campo está vacío.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            else
-            {
-                // Aqui va el codigo
-
-            }
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = $"/k Sqlcmd -S localhost\\SQLEXPRESS -E -Q \"RESTORE DATABASE [{baseName}] FROM DISK = '{rutaRestaurar}'\"";
+            System.Diagnostics.Process.Start(startInfo);
         }
     }
 }
